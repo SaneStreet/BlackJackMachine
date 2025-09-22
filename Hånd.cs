bbd74b56a02c1f07fc7baefaca0cd52dd2687786
Michael Hansen <Michael.Hansen@specialisterne.dk>
2025-09-22 14:58:37 +0200

    Tilføjet funktionalitet for kort på hånden og spiller egenskaber

--- Hånd.cs
+++ Hånd.cs
@@ -5,3 +5,35 @@
     Værdiberegninger
     Bust/Blackjack
 */
+
+class Hånd()
+{
+    //Personlig liste af kort på hånden
+    public List<Kort> kortPåHånden = new List<Kort>();
+    //Lægger kort på hånden
+    public void Add(Kort k) => kortPåHånden.Add(k);
+
+    // Udregn bedste værdi for spiller. Anse altid Es som værende 1, medmindre 11 passer bedre
+    public int VinderVærdi()
+    {
+        //Nuværende sum på hånden og antallet af Esser ("A")
+        int kortSum = kortPåHånden.Sum(k => k.Værdi);
+        int antalEsser = kortPåHånden.Count(k => k.Rank == "A");
+
+        // Laver Esser om til 11, hvis det er bedre værdi
+        while (antalEsser > 0 && kortSum + 10 <= 21)
+        {
+            kortSum += 10;
+            antalEsser--;
+        }
+        return kortSum;
+    }
+
+    public bool ErBust() => VinderVærdi() > 21;
+    public bool ErBlackjack() => kortPåHånden.Count == 2 && VinderVærdi() == 21;
+
+    public override string ToString()
+    {
+        return string.Join(" ", kortPåHånden);
+    }
+}
--- Spiller.cs
+++ Spiller.cs
@@ -1,5 +1,22 @@
 // Class : Spiller
 
 /*
-    Indeholder logik der skal holde på korthånden og udføre andre handlinger
+    Opretter spillere 
+    Tømrer kort på hånden ved hver runde
 */
+
+class Spiller
+{
+    // Henter navnet på spiller
+    public string Navn { get; }
+    // Laver en ny hånd til spilleren
+    public Hånd Hånd { get; } = new Hånd();
+
+    public Spiller(string navn)
+    {
+        Navn = navn;
+    }
+
+    // Funktion der tømmer hånden på spilleren
+    public void TømHånd() => Hånd.kortPåHånden.Clear();
+}
